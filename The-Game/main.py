31b87ef6b1e0d61af5647edacaf148268f14a30c
Jeff <31514884+GT-Gif@users.noreply.github.com>
2017-11-21 10:37:14 +0100

    ALPHA 4

--- The-Game/main.py
+++ The-Game/main.py
@@ -1,14 +1,14 @@
 #Import basic librairies
 import pyglet, sys
-from pyglet import clock
-import pygame
 #Import personal packages
 from classes import Theatre
 from constants import constants
 
 if __name__ == '__main__':
+
     Salle_de_theatre = Theatre.Salle_de_theatre()
-    pyglet.clock.set_fps_limit(constants.FRAMES_MAX)
+    pyglet.clock.set_fps_limit(60)
     pyglet.clock.schedule_interval(Salle_de_theatre.update, 1/constants.FRAMES_PER_SECOND)
+
     pyglet.app.run()
     
