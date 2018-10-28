df7f1739a92c6a94535d561dbe9243bc7e76f048
Jeff <jeanfrancois.mir.github@gmail.com>
2018-10-28 18:50:25 +0100

    camera update

---
+++ Super_Retro_World_v4/Super_Retro_World/Assets/Scenes/Behavior/CamBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBehavior : MonoBehaviour {
+
+    public int xMin;
+    public int xMax;
+    public int yMin;
+    public int yMax;
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+        //looking for one specific target to center the camera with
+        target = getNewTarget("Player");
+
+        xMin = -100;
+        xMax = 100;
+        yMin = 0;
+        yMax = 0;
+}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        float x = Mathf.Clamp(getTargetX(), xMin, xMax);
+        float y = Mathf.Clamp(getTargetY(), yMin, yMax);
+        float z = gameObject.transform.position.z;
+
+        gameObject.transform.position = new Vector3(x, y, z);
+    }
+
+
+    GameObject getNewTarget(string tag)
+    {
+        return GameObject.FindGameObjectWithTag(tag);
+    }
+
+    float getTargetX()
+    {
+        return target.transform.position.x;
+    }
+
+    float getTargetY()
+    {
+        return target.transform.position.y;
+    }
+}
